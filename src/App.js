760c46fd31f25b1c7990f5c544a8d780f43fc8ae
Ashwinbhandary99 <bring2acb@gmail.com>
2021-09-05 13:35:14 +0545

    Initialized chatfeed component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () =>{
@@ -8,8 +9,8 @@ const App = () =>{
             projectID="b03d3024-f9a2-4a0a-95d7-213463605875"
             userName="Admin"
             userSecret="123"
-
-
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>
+            }
 
 
 
