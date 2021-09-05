2638c6818eacf18cb6dc4b8ffcda116b019e6ce5
Ashwinbhandary99 <bring2acb@gmail.com>
2021-09-05 13:12:12 +0545

    Initial development

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () =>{
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="b03d3024-f9a2-4a0a-95d7-213463605875"
+            userName="Admin"
+            userSecret="123"
+
+
+
+
+
+
+
+        />
+    );
+}
+export default App;
